ae2cbc53112c1df4f78589d58b7e9f4889945bc7
Shreesh Kulkarni <kshreesh5@gmail.com>
2022-01-18 19:09:04 +0530

    Create tinyos.sh

---
+++ tinyos.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+# Here we setup the environment
+# variables needed by the tinyos
+# make system
+echo "Setting up for TinyOS 2.1.2 Repository Version"
+export TOSROOT=
+export TOSDIR=
+export MAKERULES=
+TOSROOT="/home/user/tinyos-release"
+TOSDIR="$TOSROOT/tos"
+CLASSPATH=$CLASSPATH:$TOSROOT/support/sdk/java:.:$TOSROOT/support/sdk/java/tinyos.jar
+MAKERULES="$TOSROOT/support/make/Makerules"
+export TOSROOT
+export TOSDIR
+export CLASSPATH
+export MAKERULES 
